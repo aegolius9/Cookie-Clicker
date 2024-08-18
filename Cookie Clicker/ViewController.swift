14e6742e7f1d8f556d96eb9ed311f299dfd87874
aegolius9 <aegoliuus9@gmail.com>
2024-08-17 21:08:18 -0400

    Initial Commit

---
+++ Cookie Clicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cookie Clicker
+//
+//  Created by Alex Tu on 2024-08-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
